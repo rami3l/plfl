46abcf2af22fd4a656306cdaa525a7e81265aae2
rami3l <rami3l@outlook.com>
2023-04-19 09:34:34 +0800

    feat: init project with `lake`

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «Plfl»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,18 @@
+import Lake
+open Lake DSL
+
+package plfl {
+  -- add package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+lean_lib Plfl {
+  -- add library configuration options here
+}
+
+@[default_target]
+lean_exe plfl {
+  root := `Main
+}
