c458a9134e99bdf9ff64e42d101257bab5612202
rami3l <rami3l@outlook.com>
2023-04-22 20:02:10 +0800

    feat: (wip) impl `Properties`, pt. 2

--- Plfl/Properties.lean
+++ Plfl/Properties.lean
@@ -1,14 +1,19 @@
 -- https://plfa.github.io/Properties/
 
+import Mathlib.CategoryTheory.Iso
+
 import Plfl.Lambda
 
+set_option tactic.simp.trace true
+
+open Context Context.IsTy
+
 -- https://plfa.github.io/Properties/#values-do-not-reduce
-theorem Value.not_reduce' : Value m → (Σ n, m —→ n) → False
-:= open Term.Reduce in by
-    intro v; intro ⟨n, hn⟩
-    cases v <;> try contradiction
-    · rename_i v'; cases hn
-      · rename_i n n' hn'; exact not_reduce' v' ⟨n', hn'⟩
+theorem Value.not_reduce' : Value m → (Σ n, m —→ n) → False := by
+  intro v; intro ⟨n, hn⟩
+  cases v <;> try contradiction
+  · rename_i v'; cases hn
+    · rename_i n n' hn'; exact not_reduce' v' ⟨n', hn'⟩
 
 theorem Value.not_reduce : Value m → IsEmpty (Σ n, m —→ n) :=
   Function.isEmpty (β := False) ∘ not_reduce'
@@ -20,30 +25,47 @@ theorem Reduce.not_value : m —→ n → IsEmpty (Value m) := by
   · exact ⟨n, h⟩
 
 -- https://plfa.github.io/Properties/#exercise-canonical--practice
-section canonical
--- Well-typed values must take one of a small number of canonical forms, which provide an analogue of the Value relation that relates values to their types. A lambda expression must have a function type, and a zero or successor expression must be a natural. Further, the body of a function must be well typed in a context containing only its bound variable, and the argument of successor must itself be canonical:
--- infix  4 Canonical_⦂_
-
--- data Canonical_⦂_ : Term → Type → Set where
-
---   C-ƛ : ∀ {x A N B}
---     → ∅ , x ⦂ A ⊢ N ⦂ B
---       -----------------------------
---     → Canonical (ƛ x ⇒ N) ⦂ (A ⇒ B)
-
---   C-zero :
---       --------------------
---       Canonical `zero ⦂ `ℕ
-
---   C-suc : ∀ {V}
---     → Canonical V ⦂ `ℕ
---       ---------------------
---     → Canonical `suc V ⦂ `ℕ
--- Show that Canonical V ⦂ A is isomorphic to (∅ ⊢ V ⦂ A) × (Value V), that is, the canonical forms are exactly the well-typed values.
 inductive Canonical : Term → Ty → Type where
 | can_lam : (∅ :< x ⦂ tx ⊢ n ⦂ tn) → Canonical (ƛ x : n) (tx =⇒ tn)
 | can_zero : Canonical 𝟘 ℕt
 | can_succ : Canonical n ℕt → Canonical (ι n) ℕt
-end canonical
+
+namespace Canonical
+  @[simp]
+  def well_typed_hom : Canonical v t → (∅ ⊢ v ⦂ t) × Value v := by
+    intro
+    | can_lam h => exact ⟨ty_lam h, Value.lam⟩
+    | can_zero => exact ⟨ty_zero, V𝟘⟩
+    | can_succ h => have ⟨ty, v⟩ := well_typed_hom h; exact ⟨ty_succ ty, Value.succ v⟩
+
+  @[simp]
+  def well_typed_inv : (∅ ⊢ v ⦂ t) × Value v → Canonical v t := by
+    intro
+    | ⟨ty_lam ty, Value.lam⟩ => exact can_lam ty
+    | ⟨ty_zero, Value.zero⟩ => exact can_zero
+    | ⟨ty_succ ty, Value.succ v⟩ => apply can_succ; exact well_typed_inv ⟨ty, v⟩
+
+  def hom_inv_id {v t} : @well_typed_inv v t ∘ well_typed_hom = id := by
+    funext c; cases c <;> simp_all
+    · rename_i v' c'; have := @hom_inv_id v' ℕt; apply_fun (· c') at this; trivial
+
+  def inv_hom_id {v t} : @well_typed_hom v t ∘ well_typed_inv = id := by
+    funext c; match c with
+    | ⟨ty_lam ty, Value.lam⟩ => simp_all
+    | ⟨ty_zero, Value.zero⟩ => simp_all
+    | ⟨ty_succ ty, Value.succ n⟩ =>
+        rename_i v'; have := @inv_hom_id v' ℕt;
+        rw [Function.comp_apply, well_typed_inv, well_typed_hom]; split
+        · simp_all; apply_fun (· (ty, n)) at this; simp_all
+
+  /--
+  The Canonical forms are exactly the well-typed values.
+  -/
+  instance well_typed : Canonical v t ≅ (∅ ⊢ v ⦂ t) × Value v where
+    hom := well_typed_hom
+    inv := well_typed_inv
+    hom_inv_id := hom_inv_id
+    inv_hom_id := inv_hom_id
+end Canonical
 
 -- https://plfa.github.io/Properties/#progress
