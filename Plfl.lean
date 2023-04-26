42cb68766ecbb31d8369f13f285c1a32db5d3b36
rami3l <rami3l@outlook.com>
2023-04-26 15:23:21 +0800

    feat: impl `DeBruijn`, pt. 3

--- Plfl.lean
+++ Plfl.lean
@@ -1,1 +1,9 @@
+import Mathlib.Logic.IsEmpty
+
+/--
+`is_empty` converts `IsEmpty α` to `α → False`.
+-/
+syntax "is_empty" : tactic
+macro_rules | `(tactic| is_empty) => `(tactic| apply Function.isEmpty (β := False))
+
 def hello := "world"
