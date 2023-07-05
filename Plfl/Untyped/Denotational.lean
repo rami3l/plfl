e1991c82818c910760a87e52e6dc5a365c39b4e4
rami3l <rami3l@outlook.com>
2023-07-05 20:44:10 +0800

    feat: (wip) impl `Denotational`, pt. 3

--- Plfl/Untyped/Denotational.lean
+++ Plfl/Untyped/Denotational.lean
@@ -26,15 +26,52 @@ end Notation
 
 open Notation
 
-namespace Value
-  inductive Subset : Value → Value → Type where
-  | bot : Subset ⊥ v
-  | conjL : Subset v u → Subset w u → Subset (v ⊔ w) u
-  | conjR₁ : Subset u v → Subset u (v ⊔ w)
-  | conjR₂ : Subset u w → Subset u (v ⊔ w)
-  | trans : Subset u v → Subset v w → Subset u w
-  | fn : Subset v' v → Subset w w' → Subset (v ⇾ w) (v' ⇾ w')
-  | dist : Subset (v ⇾ (w ⊔ w')) ((v ⇾ w) ⊔ (v ⇾ w'))
+inductive Subset : Value → Value → Type where
+| bot : Subset ⊥ v
+| conjL : Subset v u → Subset w u → Subset (v ⊔ w) u
+| conjR₁ : Subset u v → Subset u (v ⊔ w)
+| conjR₂ : Subset u w → Subset u (v ⊔ w)
+| trans : Subset u v → Subset v w → Subset u w
+| fn : Subset v' v → Subset w w' → Subset (v ⇾ w) (v' ⇾ w')
+| dist : Subset (v ⇾ (w ⊔ w')) ((v ⇾ w) ⊔ (v ⇾ w'))
 
-  instance : Trans Subset Subset Subset where trans := .trans
-end Value
+namespace Notation
+  scoped infix:40 " ⊑ " => Subset
+end Notation
+
+instance : Trans Subset Subset Subset where trans := .trans
+
+@[refl]
+theorem Subset.refl : v ⊑ v := match v with
+| ⊥ => .bot
+| _ ⇾ _ => .fn refl refl
+| .conj _ _ => .conjL (.conjR₁ refl) (.conjR₂ refl)
+
+/-- The `⊔` operation is monotonic with respect to `⊑`. -/
+@[simp]
+theorem subset_subset (d₁ : v ⊑ v') (d₂ : w ⊑ w') : v ⊔ w ⊑ v' ⊔ w' :=
+  .conjL (.conjR₁ d₁) (.conjR₂ d₂)
+
+@[simp]
+theorem conj_fn_conj : (v ⊔ v') ⇾ (w ⊔ w') ⊑ (v ⇾ w) ⊔ (v' ⇾ w') := calc
+  _ ⊑ ((v ⊔ v') ⇾ w) ⊔ ((v ⊔ v') ⇾ w') := .dist
+  _ ⊑ (v ⇾ w) ⊔ (v' ⇾ w') := open Subset in by
+    apply subset_subset <;> refine .fn ?_ .refl
+    · apply conjR₁; rfl
+    · apply conjR₂; rfl
+
+@[simp]
+theorem conj_subset₁ : u ⊔ v ⊑ w → u ⊑ w := by intro
+| .conjL h _ => exact h
+| .conjR₁ h => refine .conjR₁ ?_; exact conj_subset₁ h
+| .conjR₂ h => refine .conjR₂ ?_; exact conj_subset₁ h
+| .trans h h' => refine .trans ?_ h'; exact conj_subset₁ h
+
+@[simp]
+theorem conj_subset₂ : u ⊔ v ⊑ w → v ⊑ w := by intro
+| .conjL _ h => exact h
+| .conjR₁ h => refine .conjR₁ ?_; exact conj_subset₂ h
+| .conjR₂ h => refine .conjR₂ ?_; exact conj_subset₂ h
+| .trans h h' => refine .trans ?_ h'; exact conj_subset₂ h
+
+-- https://plfa.github.io/Denotational/#environments
