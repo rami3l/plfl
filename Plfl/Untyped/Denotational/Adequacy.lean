5c199fde92648d8b20945ed1e4cafd9869e60630
rami3l <rami3l@outlook.com>
2023-07-25 16:23:38 +0800

    feat: (wip) impl `Adequacy`, pt. 2

--- Plfl/Untyped/Denotational/Adequacy.lean
+++ Plfl/Untyped/Denotational/Adequacy.lean
@@ -11,17 +11,27 @@ open Untyped.Subst
 open BigStep
 open Denotational Denotational.Notation
 
-/-- `GtFn u` means that it is "greater than" a certain `.fn` value. -/
+-- https://plfa.github.io/Adequacy/#the-property-of-being-greater-or-equal-to-a-function
+/-- `GtFn u` means that it is "greater than" a certain function value. -/
 def GtFn (u : Value) : Prop := ∃ v w, v ⇾ w ⊑ u
 
-namespace GtFn
-  theorem sub (gt : GtFn u) (lt : u ⊑ u') : GtFn u' :=
-    let ⟨v, w, lt'⟩ := gt; ⟨v, w, lt'.trans lt⟩
+/-- If `u` is greater than a function, then an even greater value `u'` is too. -/
+theorem GtFn.sub (gt : GtFn u) (lt : u ⊑ u') : GtFn u' :=
+  let ⟨v, w, lt'⟩ := gt; ⟨v, w, lt'.trans lt⟩
 
-  theorem bot : ¬ GtFn ⊥ := by
-    sorry
-    -- intro ⟨v, w, lt⟩; have := sub_inv_fun lt
-end GtFn
+/-- `⊥` is never greater than a function. -/
+lemma not_gtFn_bot : ¬ GtFn ⊥
+| ⟨v, w, lt⟩ => by
+  have ⟨_, f, s, _⟩ := sub_inv_fn lt; have ⟨_, _, i⟩ := elem_of_allFn f; cases s i
+
+/-- If the join of two values is greater than a function, then at least one of them is too. -/
+theorem GtFn.conj (gt : GtFn (u ⊔ v)) : GtFn u ∨ GtFn v := by
+  have ⟨_, _, lt⟩ := gt; have ⟨_, f, s, _⟩ := sub_inv_fn lt; have ⟨v, w, i⟩ := elem_of_allFn f
+  refine Or.imp ?inl ?inr <| s i <;> (intro i'; exists v, w; exact sub_of_elem i')
+
+/-- If neither of the two values is greater than a function, then nor is their join. -/
+lemma not_gtFn_conj (ngt : ¬ GtFn u) (ngt' : ¬ GtFn v) : ¬ GtFn (u ⊔ v) := by
+  intro gtuv; exfalso; exact (gtuv.conj).elim ngt ngt'
 
 namespace Notation
 end Notation
